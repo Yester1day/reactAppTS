d82b9c785881e166cceb29167161ede2a9aa3b80
Yeste1day <dima1999by@mail.ru>
2022-03-31 19:00:29 +0300

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -14,8 +14,8 @@ function App(props: any ) {
     return (
 
         <div>
-            <PageTitle title={"This is APP component"}/>
-            <PageTitle title={"My friends"}/>
+            <PageTitle title={"This is  APP  component"}/>
+            <PageTitle title={"Mygit sttauas friends"}/>
             Article 1
             <Rating value={1}/>
             <Accordion titleValue={"Menu"} />
